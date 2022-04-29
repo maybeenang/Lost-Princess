924d424b14bee53ffc442de643dd5592bfe7b048
henhen02 <hendri21072002@gmail.com>
2022-04-30 02:02:15 +0700

    update class menu

---
+++ Module/Menu.py
@@ -0,0 +1,28 @@
+class Menu:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def options(self):
+        pass
+
+    def about(self):
+        pass
+
+    def quit(self):
+        pass
+
+class InGameMenu(Menu):
+    def __init__(self):
+        super().__init__()
+
+    def resume(self):
+        pass
+
+    def main_menu(self):
+        pass
+
+    def quit_game(self):
+        pass
