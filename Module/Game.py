7f2bdc0799f5763ef29ef6889ccbbaa35de30327
elang194 <muhammad.120140194@student.itera.ac.id>
2022-05-14 00:08:57 +0700

    commit 11 may

--- Module/Game.py
+++ Module/Game.py
@@ -4,7 +4,6 @@ from Assets.Settings import *
 from Assets.Level_set import *
 from Module.Level import *
 from Module.MenuPack.MainMenu import *
-from Module.MenuPack.Options import *
 from Assets.Menu_set import *
 
 class Game:
@@ -14,11 +13,9 @@ class Game:
         self.clock = pygame.time.Clock()
         self.level = Level(LEVEL_1, self.screen)
         self.menu = MainMenu(self.screen)
-        self.opt = Options(self.screen)
 
         self.running = True
         self.cek_menu = True
-        self.submenu = 'main'
         self.run()
     
     
@@ -29,42 +26,57 @@ class Game:
                 self.running = False
                 sys.exit()
             
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    if not self.cek_menu:
+                        if self.level.status:
+                            self.level.status = False
+                        else:
+                            self.level.status = True
+            
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pointer = pygame.mouse.get_pos()
-
-                if self.submenu == 'main':
+                if self.menu.status == 'main':
                     if self.menu.cek_button(pointer) == 'start':
                         self.cek_menu = False
                     elif self.menu.cek_button(pointer) == 'opt':
-                        self.submenu = 'opt'
+                        self.menu.status = 'opt'
                     elif self.menu.cek_button(pointer) == False:
                         self.running = False
                         sys.exit()
-
-                elif self.submenu == 'opt':
-                    if self.opt.cek_button(pointer) == 'back':
-                        self.submenu = 'main'
+                elif self.menu.status == 'opt':
+                    if self.menu.cek_button(pointer) == 'backopt':
+                        self.menu.status = 'main'
+                
+                if not self.cek_menu:
+                    if self.level.status:
+                        if self.level.pause_game(pointer):
+                            self.level.status = False
+                    else:
+                        if self.level.pause.cek_button(pointer) == 'resume':
+                            self.level.status = True
+                        elif self.level.pause.cek_button(pointer) == 'quit':
+                            self.menu.status = 'main'
+                            self.cek_menu = True
 
     def drawmenu(self):
-        if self.submenu == 'main':
-            self.menu.draw()
-        elif self.submenu == 'opt':
-            self.opt.draw()
+        self.menu.draw()
     
     def draw(self):
-        self.screen.fill('grey')
+        self.screen.fill('black')
         self.level.draw()
         if self.level.player.sprite.health <= 0:
             self.cek_menu = True
 
     def run(self):
+        
         while self.running:
             self.events()
 
             if self.cek_menu:
                 self.drawmenu()
             else:
-                self.bg.fadeout(1000)
+                self.menu.bg_sound.fadeout(1000)
                 self.draw()
 
             pygame.display.update()
