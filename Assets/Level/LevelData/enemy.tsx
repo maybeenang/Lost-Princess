1f9bd6cd1393c0ad7a5953cb796157e78d8cd89f
elang194 <muhammad.120140194@student.itera.ac.id>
2022-05-25 22:48:56 +0700

    merapihkan folder

---
+++ Assets/Level/LevelData/enemy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="enemy" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../Graphics/enemy.png" trans="000000" width="32" height="64"/>
+</tileset>
---
+++ Assets/Level/LevelData/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="floor" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="../Graphics/floor.png" trans="000000" width="320" height="64"/>
+</tileset>
