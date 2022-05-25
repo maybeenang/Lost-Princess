b5e53e2ab30b5eb485f41e621afb54e06ab70a52
elang194 <muhammad.120140194@student.itera.ac.id>
2022-05-25 22:48:31 +0700

    merapihkan folder

--- Assets/Level/pisang.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="pisang" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Graphics/pisang.png" trans="000000" width="32" height="32"/>
-</tileset>
