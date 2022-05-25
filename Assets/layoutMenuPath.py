43bbbf54564112e4800c6c30a332352ff5b06a93
elang194 <muhammad.120140194@student.itera.ac.id>
2022-05-25 20:53:10 +0700

    merapihkan penamaan

---
+++ Assets/layoutMenuPath.py
@@ -0,0 +1,6 @@
+layoutMenuPath = {
+    'logotim': 'Assets/LayoutMenu/logotim.png',
+    'tombolpositive': 'Assets/LayoutMenu/tombolpositive.png',
+    'tombolnegative': 'Assets/LayoutMenu/tombolnegative.png',
+    'font': 'Assets/LayoutMenu/font.ttf'
+}
---
+++ Assets/soundPath.py
@@ -0,0 +1,8 @@
+soundPath = {
+    "click": "Assets/Sound/click.wav",
+    "positive_click": "Assets/Sound/positiveclick.wav",
+    "negative_click": "Assets/Sound/negativeclick.wav",
+    "error_click": "Assets/Sound/errorclick.wav",
+    "inmenubacksound": "Assets/Sound/inmenubacksound.wav",
+    "ingamebacksound": "Assets/Sound/ingamebacksound.wav",    
+}
