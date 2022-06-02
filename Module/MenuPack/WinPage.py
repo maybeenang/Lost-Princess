42f8205ab63a45534e24aa57e0b928c24c833f1d
elang194 <muhammad.120140194@student.itera.ac.id>
2022-06-03 05:33:42 +0700

    update state game

--- Module/MenuPack/WinPage.py
+++ Module/MenuPack/WinPage.py
@@ -1,4 +1,4 @@
-from Assets.Settings import WIDTH
+from Assets.Settings import HEIGHT, WIDTH
 from Module.Menu import *
 from Module.MenuPack.Button import *
 from Assets.layoutMenuPath import *
@@ -12,15 +12,25 @@ class WinPage(Menu):
         self.nextlevel = nextlevel 
 
         # font
-        self.font = pygame.font.Font(layoutMenuPath['font'], 30)
+        self.font = pygame.font.Font(layoutMenuPath['font'], 15)
 
 
         # text
-        self.text = "You Won"
-        self.textrender = self.font.render(self.text, True, (255, 255, 255))
-        self.textrect = self.textrender.get_rect(center=(WIDTH/2, 75))
-        self.leveltext = self.font.render("New Level Unlocked", True, (255, 255, 255))
-        self.leveltextrect = self.leveltext.get_rect(center=(WIDTH/2, 125))
+
+        if self.nextlevel == 1:
+            self.textatas = "You Won"
+            self.textbawah = "You have finished the tutorial"
+        elif self.nextlevel > 1 and self.nextlevel < 5:
+            self.textatas = "You Won"
+            self.textbawah = "You Unlocked The Next Level"
+        elif self.nextlevel == 5:
+            self.textatas = "Congratulations"
+            self.textbawah = "You have successfully saved the princess"
+
+        self.textrender = self.font.render(self.textatas, True, (255, 255, 255))
+        self.textrect = self.textrender.get_rect(center=(WIDTH/2, HEIGHT/2 - 25))
+        self.leveltext = self.font.render(self.textbawah, True, (255, 255, 255))
+        self.leveltextrect = self.leveltext.get_rect(center=(WIDTH/2, HEIGHT/2))
     
     def draw(self):
         self.surface.fill((0, 0, 0))
