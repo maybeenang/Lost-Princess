1ff46d9128e5b0a90957f021fb9339cdcfa6b8c6
maybeenang <muhammad.120140194@student.itera.ac.id>
2022-06-04 22:07:21 +0700

    Done

--- Assets/layoutMenuPath.py
+++ Assets/layoutMenuPath.py
@@ -1,13 +1,13 @@
 layoutMenuPath = {
-    'bg': 'Assets/LayoutMenu/background.png',
-    'frame': 'Assets/layoutMenu/frame.png',
-    'framelevel': 'Assets/layoutMenu/framelevel.png',
-    'frameoption': 'Assets/layoutMenu/frameoption.png',
-    'framecredits': 'Assets/layoutMenu/framecredits.png',
-    'logotim': 'Assets/LayoutMenu/logotim.png',
-    'tombolpositive': 'Assets/LayoutMenu/tombolpositive.png',
-    'tombolnegative': 'Assets/LayoutMenu/tombolnegative.png',
-    'tombolhover': 'Assets/LayoutMenu/tombolhover.png',
-    'tombolnegativehover': 'Assets/LayoutMenu/tombolnegativehover.png',
-    'font': 'Assets/LayoutMenu/font.ttf'
+    'bg': './Assets/LayoutMenu/background.png',
+    'frame': './Assets/LayoutMenu/frame.png',
+    'framelevel': './Assets/LayoutMenu/framelevel.png',
+    'frameoption': './Assets/LayoutMenu/frameoption.png',
+    'framecredits': './Assets/LayoutMenu/framecredits.png',
+    'logotim': './Assets/LayoutMenu/logotim.png',
+    'tombolpositive': './Assets/LayoutMenu/tombolpositive.png',
+    'tombolnegative': './Assets/LayoutMenu/tombolnegative.png',
+    'tombolhover': './Assets/LayoutMenu/tombolhover.png',
+    'tombolnegativehover': './Assets/LayoutMenu/tombolnegativehover.png',
+    'font': './Assets/LayoutMenu/font.ttf'
 }
